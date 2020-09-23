08d92be9413d1563fe96b0cc3e305b3128bdd4f8
Utsav Tulsyan <utsav.tulsyan@publicissapient.com>
2020-09-24 00:54:37 +0530

    add main screen

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        imageView.image = ballArray[getRandom()]
+    }
+    
+    func getRandom() -> Int {
+        return Int.random(in: 0...4)
+    }
 }
 
